d69a73f0bf660cf4a28ffb2046f4c0215ebdbcf1
Prem Deshmukh <Premdeshmukh1818@gmail.com>
2023-05-06 18:19:45 +0530

    E

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Cards from './compo/Cards'
+import Navbar from './compo/Navbar'
+import Filter from './compo/Filter'
+import Spinner from './compo/Spinner'
+import {apiUrl ,filterData} from './data'
+import { useState } from 'react';
+import { toast } from 'react-toastify';
+import { useEffect } from 'react';
+
 
 function App() {
+ const [courses,setCourses]=useState(null);
+ const [loading,setLoading]=useState(true);
+  async function fetchData(){
+
+    setLoading(true);
+    try{
+      let response = await fetch(apiUrl);
+      let output = await response.jsson();
+
+      setCourses(output);
+    }
+    catch(error){
+       toast.error("something is wrong");
+
+    }
+    setLoading(false); 
+  }
+
+  useEffect(() => {
+    fetchData();
+  
+  }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+    <div>
+      <Navbar/>
+    </div>
+    <div>
+      <Filter filterData={filterData}/>
+    </div>
+    <div>
+      {loading?(<Spinner/>): (<Cards courses={courses}/>)} 
+    </div>
+    <div>
+
+    </div>
     </div>
   );
 }
---
+++ src/compo/Spinner.js
@@ -0,0 +1,12 @@
+ import React from 'react'
+ import './Spinner.css'
+ const Spinner = () => {
+   return (
+     <div>
+       <div class="custom-loader"></div>
+        <p>Loading.......</p>
+     </div>
+   )
+ }
+ 
+ export default Spinner
